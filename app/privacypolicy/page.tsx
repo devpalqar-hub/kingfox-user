6f7316e5b0be215766ac47288246ac1021f87ab0
mudfayaz44 <muhdfayaz44@gmail.com>
2026-08-12 20:34:15 +0530

    number changed

--- app/privacypolicy/page.tsx
+++ app/privacypolicy/page.tsx
@@ -142,7 +142,7 @@ export default function PrivacyPolicy() {
         <strong>Business Address:</strong> KINGFOX CLOTHING STORE, MN'S AVENUE,
         Calicut, Kerala
         <br />
-        <strong>Phone:</strong> 8136882245
+        <strong>Phone:</strong> 8129882245
       </p>
 
       <h2>RETURN & REFUND POLICY</h2>
